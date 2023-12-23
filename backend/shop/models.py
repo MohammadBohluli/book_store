5bb81414112cb07ce03539f5e783573b79a18701
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-12-23 16:59:13 +0330

    create book app models

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_add_now=True)
+    updated = models.DateTimeField(auto_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Book(TimeStampedModel):
+    class Language(models.TextChoices):
+        Persian = "fr", _("Persian")
+        English = "en", _("English")
+
+    title = models.CharField(max_length=255)
+    isbn = models.CharField(
+        max_length=10,
+    )
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField()
+    num_pages = models.PositiveIntegerField()
+    language = models.CharField(
+        max_length=2, choices=Language, default=Language.English
+    )
+    category = models.ManyToManyField("Category")
+    publisher = models.ManyToManyField("Publisher")
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Category(TimeStampedModel):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Publisher(TimeStampedModel):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
