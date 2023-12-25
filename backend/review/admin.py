05a910f6e46cf5c8eafba1eacb443637752eee73
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-12-25 20:25:54 +0330

    create admin review app

--- backend/review/admin.py
+++ backend/review/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
+
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["name", "date_created", "book"]
+
+
+admin.site.register(Review, ReviewAdmin)
