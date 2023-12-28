092710bdde933a71d27b72cbc2f206bd27251910
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-12-28 12:30:58 +0330

    create admin panel for customer

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Customer
 
 
 class UserAdmin(BaseUserAdmin):
@@ -26,4 +26,12 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "birth_date",
+    ]
+
+
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Customer, CustomerAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -57,3 +57,6 @@ class Customer(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
