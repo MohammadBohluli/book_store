263f9b56a82ef7a3a4c27f63c08dc83ea7349aef
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-12-28 12:49:20 +0330

    create router for CustomerViewSet

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,6 +3,8 @@ from .models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
     class Meta:
         model = Customer
         fields = ["id", "user_id", "phone", "birth_date"]
--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
 from review.views import ReviewViewSet
+from accounts.views import CustomerViewSet
 from . import views
 
 
@@ -10,6 +11,7 @@ router = DefaultRouter()
 router.register("books", views.BookViewSet, basename="book")
 router.register("categories", views.CategoryViewSet, basename="category")
 router.register("carts", views.CartViewSet, basename="cart")
+router.register("customers", CustomerViewSet, basename="customer")
 
 books_router = routers.NestedSimpleRouter(router, "books", lookup="book")
 books_router.register("reviews", ReviewViewSet, basename="book-reviews")
